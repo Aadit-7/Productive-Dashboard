e78193deadbab804ace601c9531431d7faecf0b5
Aadit Yadav <aadityadav121@gmail.com>
2025-12-22 20:45:49 +0530

    To Do List functionality halfly done

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+function customMouse() {
+  let mouse = document.querySelector("main");
+
+  mouse.addEventListener("mousemove", (e) => {
+    mouse.style.setProperty("--x", e.clientX + "px");
+    mouse.style.setProperty("--y", e.clientY + "px");
+    console.log(e);
+  });
+}
+
+
 function openCloseCardFeature() {
   let allElems = document.querySelectorAll(".elem");
   let allFullElem = document.querySelectorAll(".fullElems");
@@ -20,3 +31,61 @@ function openCloseCardFeature() {
   });
 }
 openCloseCardFeature();
+
+var currentTask = [];
+
+if (localStorage.getItem("currentTask")) {
+  currentTask = JSON.parse(localStorage.getItem("currentTask"));
+} else {
+  console.log("To Do List is empty");
+}
+function renderTask() {
+  let allTasks = document.querySelector(".all-task");
+  let sum = "";
+  // console.log(allTasks);
+
+  currentTask.forEach((elem, idx) => {
+    // console.log(elem);
+    sum += `<div class="task">
+              <h5>${elem.title}<span class="${elem.imp}">*</span></h5>
+              <button id="${idx}">Mark as done</button>
+            </div>`;
+  });
+
+  // console.log(sum);
+  allTasks.innerHTML = sum;
+  localStorage.setItem("currentTask", JSON.stringify(currentTask));
+}
+renderTask();
+
+let form = document.querySelector(".add-task form");
+let formInput = document.querySelector(".add-task form #task-input");
+let formTextarea = document.querySelector(".add-task form textarea");
+let formCheckbox = document.querySelector(".add-task form #check");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  // console.log(formInput.value);
+  // console.log(formTextarea.value);
+  // console.log(formCheckbox.checked);
+
+  currentTask.push({
+    title: formInput.value,
+    description: formTextarea.value,
+    imp: formCheckbox.checked,
+  });
+  // console.log(currentTask);
+  renderTask();
+  location.reload();
+});
+
+let markDoneBtn = document.querySelectorAll(".task button");
+
+markDoneBtn.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    currentTask.splice(btn.id, 1);
+    renderTask();
+    location.reload();
+  });
+});
